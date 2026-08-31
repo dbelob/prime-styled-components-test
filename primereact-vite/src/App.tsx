2120483bc2256314b3f5ae123a50bfeaf8c3743a
Dmitry Beloborodov <dbelob@users.noreply.github.com>
2026-08-31 14:01:39 +0300

    Add InputText demo

--- primereact-vite/src/App.tsx
+++ primereact-vite/src/App.tsx
@@ -1,12 +1,16 @@
 import { Button } from "@primereact/ui/button";
+import { InputText } from "@primereact/ui/inputtext";
 import "./App.css";
 
 export default function App() {
     return (
         <>
             <h1>PrimeReact (without Tailwind)</h1>
-            <div>
+            <div style={{ margin: "1rem 0 1rem 0" }}>
                 <Button>Check</Button>
+            </div>
+            <div style={{ margin: "1rem 0 1rem 0" }}>
+                <InputText placeholder="Enter text" />
             </div>
         </>
     );
